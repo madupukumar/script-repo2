5a3fe73e0cf60f467e4824b9fa21241ddfe1e8e5
madupukumar <madupusathish465@gmail.com>
2024-08-23 11:17:19 +0530

    msk1

---
+++ multipication.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user for two numbers
+echo "Enter the first number:8"
+
+echo "Enter the second number:9"
+
+
+# Perform multiplication
+result=$((8 * 9))
+
+# Display the result
+echo "The multiplication of $8 and $9 is: $result"
